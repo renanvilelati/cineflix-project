c1aa401657843ba3aecabff457540716007174e8
renanvilelati <renanvilelati@gmail.com>
2023-05-07 23:17:35 -0300

    refactor: Remove footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Outlet } from 'react-router-dom'
-import { Header } from './components/Header'
+import { Outlet } from 'react-router-dom';
+import { Header } from './components/Header';
 
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
-
   return (
     <>
       <ToastContainer autoClose={3000} />
       <Header />
       <Outlet />
-      <footer>Footer</footer>
     </>
-  )
-}
+  );
+};
