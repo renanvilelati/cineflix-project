8e59218275ea54207818b10c01b61cc01ca42368
renanvilelati <renanvilelati@gmail.com>
2023-05-07 23:46:08 -0300

    feat: Change API language

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,60 +1,52 @@
-import { useEffect, useState } from "react"
-import { api } from "../../services/api"
-import { Link } from "react-router-dom"
-import './styles.css'
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 export const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    // https://api.themoviedb.org/3/movie/550?api_key=916b3d045649e2789aeb4cab5a2dc88d
+  useEffect(() => {
+    async function loadMovies() {
+      const response = await api.get('movie/now_playing', {
+        params: {
+          api_key: '916b3d045649e2789aeb4cab5a2dc88d',
+          page: 1,
+        },
+      });
 
-    const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-
-        async function loadMovies() {
-            const response = await api.get('movie/now_playing', {
-                params: {
-                    api_key: '916b3d045649e2789aeb4cab5a2dc88d',
-                    language: 'pt-BR',
-                    page: 1
-                }
-            })
-
-            setMovies(response.data.results.slice(0, 10))
-            setLoading(false)
-        }
-
-        loadMovies()
-
-    }, [])
-
-    if (loading) {
-        return (
-            <div className="loading">
-                <h2>Carregando conteúdo...</h2>
-            </div>
-        )
+      setMovies(response.data.results.slice(0, 12));
+      setLoading(false);
     }
 
+    loadMovies();
+  }, []);
 
+  if (loading) {
     return (
-        <div className="container">
-            <div className="movie-list">
-                {
-                    movies.map(movie => {
-                        return (
-                            <article key={movie.id}>
-                                <strong>{movie.title}</strong>
-                                <img
-                                    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                                    alt={movie.title} />
-                                <Link to={`movie/${movie.id}`}>Acessar</Link>
-                            </article>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+      <div className='loading'>
+        <h2>Carregando conteúdo...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className='container'>
+      <div className='movie-list'>
+        {movies.map((movie) => {
+          return (
+            <article key={movie.id}>
+              <strong>{movie.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <Link to={`movie/${movie.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,99 +1,93 @@
-import { useEffect, useState, } from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import { api } from "../../services/api"
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
 
-import { toast } from "react-toastify"
+import { toast } from 'react-toastify';
 
-import './styles.css'
+import './styles.css';
 
 export const Movies = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const { id } = useParams()
-    const navigate = useNavigate()
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const [movie, setMovie] = useState({})
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        async function loadMovie() {
-            const response = await api.get(`/movie/${id}`, {
-                params: {
-                    api_key: '916b3d045649e2789aeb4cab5a2dc88d',
-                    language: 'pt-BR'
-                }
-            })
-                .then(response => {
-                    setMovie(response.data)
-                    setLoading(false)
-                })
-                .catch(error => {
-                    console.log('Movie not found!');
-                    navigate('/', { replace: true })
-                    return
-                })
-        }
-
-        loadMovie()
-
-        return () => {
-            console.log('Disassembled component!');
-        }
-    }, [navigate, id])
-
-
-
-
-    const handleSaveMovie = () => {
-        // are movies saved in the localStorage?
-        const myMovieList = localStorage.getItem('@cineflix')
-
-        // if there are, parse it into JSON, if not, create an array
-        let savedMovies = JSON.parse(myMovieList) || []
-
-        const hasMovie = savedMovies.some((movieSaved) => movieSaved.id === movie.id)
-
-        if (hasMovie) {
-            toast.warning('Movie already saved in the list')
-            return
-        }
-
-        savedMovies.push(movie)
-        localStorage.setItem('@cineflix', JSON.stringify(savedMovies))
-        toast.success('Successfully saved movie!')
-
+  useEffect(() => {
+    async function loadMovie() {
+      const response = await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: '916b3d045649e2789aeb4cab5a2dc88d',
+          },
+        })
+        .then((response) => {
+          setMovie(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log('Movie not found!');
+          navigate('/', { replace: true });
+          return;
+        });
     }
 
+    loadMovie();
 
+    return () => {
+      console.log('Disassembled component!');
+    };
+  }, [navigate, id]);
 
+  const handleSaveMovie = () => {
+    // are movies saved in the localStorage?
+    const myMovieList = localStorage.getItem('@cineflix');
 
-    if (loading) {
-        return (
-            <div className="movie-info">
-                <h2>Loading movie details...</h2>
-            </div>
-        )
+    // if there are, parse it into JSON, if not, create an array
+    let savedMovies = JSON.parse(myMovieList) || [];
+
+    const hasMovie = savedMovies.some(
+      (movieSaved) => movieSaved.id === movie.id
+    );
+
+    if (hasMovie) {
+      toast.warning('Movie already saved in the list');
+      return;
     }
 
+    savedMovies.push(movie);
+    localStorage.setItem('@cineflix', JSON.stringify(savedMovies));
+    toast.success('Successfully saved movie!');
+  };
+
+  if (loading) {
     return (
-        <div className="movie-info">
-            <h2>{movie.title}</h2>
-            <img
-                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                alt={movie.title} />
-            <h3>Overview</h3>
-            <p>
-                {movie.overview}
-            </p>
-            <strong>Average: {movie.vote_average} / 10</strong>
-            <div className="area-buttons">
-                <button onClick={handleSaveMovie}>Save</button>
-                <button>
-                    <a
-                        href={`https://youtube.com/results?search_query=${movie.title} trailer`}
-                        rel='external'
-                        target='_blank'> Trailer </a>
-                </button>
-            </div>
-        </div>
-    )
-}
+      <div className='movie-info'>
+        <h2>Loading movie details...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className='movie-info'>
+      <h2>{movie.title}</h2>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt={movie.title}
+      />
+      <h3>Overview</h3>
+      <p>{movie.overview}</p>
+      <strong>Average: {movie.vote_average} / 10</strong>
+      <div className='area-buttons'>
+        <button onClick={handleSaveMovie}>Save</button>
+        <a
+          href={`https://youtube.com/results?search_query=${movie.title} trailer`}
+          rel='external'
+          target='_blank'
+        >
+          Trailer
+        </a>
+      </div>
+    </div>
+  );
+};
